f196e9cd3ce5764ec05ad706621425e659dd4a1a
Yoav-Koursh <yoav.koursh@gmail.com>
2023-07-17 21:24:32 +0300

    work

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var i=100
 function changebackgroundcolor(){
 // if (document.body.style.backgroundColor== "blue"){
 //  document.body.style.backgroundColor = "red"; }
@@ -5,6 +6,48 @@ function changebackgroundcolor(){
 // }
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
 var randomColor= "#"+ randomColor
-console.log (randomColor)
 document.body.style.backgroundColor=randomColor;
 }
+
+function waitback(){
+setTimeout(()=>{
+waitback()
+changebackgroundcolor()
+},"1000")
+}
+
+function transparent(){
+
+document.getElementById('img').style.opacity=i/100;
+i=i-5
+}
+
+function waittrans(){
+setTimeout(()=>{
+if (i>50)
+{waittrans()
+transparent()}
+},"10")
+}
+function nontransparent(){
+
+document.getElementById('img').style.opacity=i/100;
+i=i+5
+}
+
+function waitnottrans(){
+setTimeout(()=>{
+if (i<105)
+{waitnottrans()
+nontransparent()}
+},"10")
+}
+
+
+
+// function jump_up_and_down() {
+// var hight= document.getElementById("img").style.top;
+// console.log (hight);
+//  document.getElementById("img").top = "100px"; 
+// 	}
+// couldnt figure it out :(
